66a7475a05d50d45ad6e4b3be32fc59fc94f7eed
Perlverity <snowman.ss.6124@gmail.com>
2023-01-21 17:23:10 +0900

    #03: feat: done 1.4.3

--- snippets/views.py
+++ snippets/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from snippets.models import Snippet  # Snippet モデルをインポート
 
@@ -19,4 +19,5 @@ def snippet_edit(request, snippet_id):
 
 
 def snippet_detail(request, snippet_id):
-    return HttpResponse('スニペットの詳細閲覧')
+    snippet = get_object_or_404(Snippet, pk=snippet_id)
+    return render(request, 'snippets/snippet_detail.html', {'snippet': snippet})
