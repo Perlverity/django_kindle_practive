c55da03d1849899aa4a06684ff58e1c644092593
Perlverity <snowman.ss.6124@gmail.com>
2023-01-21 17:11:01 +0900

    #02: feat: 1.4.2 done.

--- snippets/tests.py
+++ snippets/tests.py
@@ -1,19 +1,27 @@
-from django.test import TestCase
+from django.test import TestCase, Client, RequestFactory
 from django.http import HttpRequest
 from django.urls import resolve
+from django.contrib.auth import get_user_model
 
 from snippets.views import top, snippet_new, snippet_edit, snippet_detail
 
+from snippets.models import Snippet
+from snippets.views import top
+
+UserModel = get_user_model()
+
 
 # Create your tests here.
+# トップページがb"HelloWorld"というプレーンテキストではなくHTMLファイルを返すように変更するため、
+# #TopPageViewTestクラスは削除して、TopPageTestクラスを次のように書き換え。
 class TopPageTest(TestCase):
-    def test_top_returns_200(self):
+    def test_top_page_returns_200_and_expected_title(self):
         response = self.client.get('/')
-        self.assertEqual(200, response.status_code)
+        self.assertContains(response, "Djangoスニペット", status_code=200)
 
-    def test_top_returns_expected_content(self):
+    def test_top_page_uses_expected_template(self):
         response = self.client.get('/')
-        self.assertEqual(b"Hello World", response.content)
+        self.assertTemplateUsed(response, "snippets/top.html")
 
 
 class CreateSnippetTest(TestCase):
@@ -32,3 +40,30 @@ class EditSnippetTest(TestCase):
     def test_should_resolve_snippet_edit(self):
         found = resolve("/snippets/1/edit/")
         self.assertEqual(snippet_edit, found.func)
+
+
+class TopPageRenderSnippetsTest(TestCase):
+    def setUp(self):
+        self.user = UserModel.objects.create(
+            username='test_user',
+            email='test@example.com',
+            password='top_secret_pass0001',
+        )
+        self.snippet = Snippet.objects.create(
+            title='title1',
+            code="print('hello')",
+            description='description1',
+            created_by=self.user,
+        )
+
+    def test_should_return_snippet_title(self):
+        request = RequestFactory().get('/')
+        request.user = self.user
+        response = top(request)
+        self.assertContains(response, self.snippet.title)
+
+    def test_should_return_username(self):
+        request = RequestFactory().get('/')
+        request.user = self.user
+        response = top(request)
+        self.assertContains(response, self.user.username)
--- snippets/views.py
+++ snippets/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from snippets.models import Snippet  # Snippet モデルをインポート
 
 
 # Create your views here.
 def top(request):
-    return HttpResponse(b"Hello World")
+    snippets = Snippet.objects.all()  # 一覧を取得
+    context = {"snippets": snippets}  # テンプレートエンジンに与えるオブジェクト
+    return render(request, "snippets/top.html", context)
 
 
 def snippet_new(request):
